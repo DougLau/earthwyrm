0285a755981b97bbeaa4898ee5ba56864815214f
Doug Lau <doug.p.lau@gmail.com>
2022-10-19 10:25:43 -0500

    Improved variable names

--- earthwyrm-bin/static/install.sh
+++ earthwyrm-bin/static/install.sh
@@ -3,22 +3,22 @@
 SRC=$(dirname "$0")
 BIN_DIR=/usr/local/bin
 ETC_DIR=/etc/earthwyrm
-VAR_DIR=/var/local/earthwyrm
-OSM_DIR=$VAR_DIR/osm
-STATIC_DIR=$VAR_DIR/static
-LOAM_DIR=$VAR_DIR/loam
+BASE_DIR=/var/local/earthwyrm
+OSM_DIR=$BASE_DIR/osm
+STATIC_DIR=$BASE_DIR/static
+LOAM_DIR=$BASE_DIR/loam
 SRV_DIR=/etc/systemd/system
 
 cp $SRC/../target/release/earthwyrm $BIN_DIR &&
 useradd --system earthwyrm &&
 mkdir $ETC_DIR &&
 cp $SRC/earthwyrm.muon $ETC_DIR &&
-mkdir $VAR_DIR &&
+mkdir $BASE_DIR &&
 mkdir $OSM_DIR &&
 mkdir $STATIC_DIR &&
 mkdir $LOAM_DIR &&
 cp $SRC/index.html $STATIC_DIR &&
 cp $SRC/map.* $STATIC_DIR &&
-chown --recursive earthwyrm.earthwyrm $VAR_DIR &&
+chown --recursive earthwyrm.earthwyrm $BASE_DIR &&
 cp $SRC/earthwyrm.service $SRV_DIR &&
 echo "Success!"
